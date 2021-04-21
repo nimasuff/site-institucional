6d81a69344b5e93a7d8367045dbf04a5150ff567
jgabrielgm <jgabrielgm1@gmail.com>
2021-04-20 23:40:33 -0300

    Início do JS de Membros

---
+++ membros.js
@@ -0,0 +1,7 @@
+function esquerdaPerfil(){
+    var pesquisadores_uff = ['Alessandro Velário','Emmanoel de Oliveira','Manoel Tabet Soriano','Roldan Petros Muradian Sarache','Teresa Bezerra Meira', 'Carolina Grottera']
+    var i;
+    for (i = 0; ){
+
+    }
+}
