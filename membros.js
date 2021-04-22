c715631123459de2129b89ac10bdd0e7d9561802
jgabrielgm <jgabrielgm1@gmail.com>
2021-04-22 00:22:47 -0300

    Fim 22/04/21

--- membros.js
+++ membros.js
@@ -1,14 +1,245 @@
-function esquerdaPerfil(){
-    var membro_AV = {
-        nome_completo = 'Alessandro Valério',
-        foto_perfil = '',
+// definindo as variaveis para coordenadores
+var membros_coordenador = [
+    {
+        id = 1,
+        nome = 'Claude Cohen',
+        img_perfil = '',
         linkedin = '',
-        lattes = 'http://lattes.cnpq.br/8542656790521330'
+        lattes = ''
     }
-    
-    var nome_pesq_int = ['Alessandro Velário','Emmanoel de Oliveira','Manoel Tabet Soriano','Roldan Petros Muradian Sarache','Teresa Bezerra Meira', 'Carolina Grottera']
-    var i;
-    for (i = 0; ){
+]
+// definindo as variaveis para pesquisadores internos
+var membros_pesq_int = [
+    {
+        id = 1,
+        nome = '',
+        img_perfil = '',
+        linkedin = '',
+        lattes = '',
+    },
+    {
+        id = 2,
+        nome = '',
+        img_perfil = '',
+        linkedin = '',
+        lattes = '',
+    },
+    {
+        id = 3,
+        nome = '',
+        img_perfil = '',
+        linkedin = '',
+        lattes = '',
+    },
+    {
+        id = 4,
+        nome = '',
+        img_perfil = '',
+        linkedin = '',
+        lattes = '',
+    },
+    {
+        id = 5,
+        nome = '',
+        img_perfil = '',
+        linkedin = '',
+        lattes = '',
+    },
+    {
+        id = 6,
+        nome = '',
+        img_perfil = '',
+        linkedin = '',
+        lattes = '',
+    },
+]
+// definindo as variaveis para pesquisadores externos
+var membros_pesq_ext = [
+    {
+        id = 1,
+        nome = '',
+        img_perfil = '',
+        linkedin = '',
+        lattes = '',
+    },
+    {
+        id = 2,
+        nome = '',
+        img_perfil = '',
+        linkedin = '',
+        lattes = '',
+    },
+    {
+        id = 3,
+        nome = '',
+        img_perfil = '',
+        linkedin = '',
+        lattes = '',
+    },
+    {
+        id = 4,
+        nome = '',
+        img_perfil = '',
+        linkedin = '',
+        lattes = '',
+    },
+    {
+        id = 5,
+        nome = '',
+        img_perfil = '',
+        linkedin = '',
+        lattes = '',
+    },
+    {
+        id = 6,
+        nome = '',
+        img_perfil = '',
+        linkedin = '',
+        lattes = '',
+    },
+]
+// definindo as variaveis para estudantes de economia
+var bach_economia = [
+    {
+        id = 1,
+        nome = 'João Gabriel Martins',
+        img_perfil = '\img\membros\jg.jpg',
+        linkedin = 'https://www.linkedin.com/in/joao-gabriel-martins/',
+        lattes = 'http://lattes.cnpq.br/8542656790521330',
+    },
+    {
+        id = 2,
+        nome = '',
+        img_perfil = '',
+        linkedin = '',
+        lattes = '',
+    },
+    {
+        id = 3,
+        nome = '',
+        img_perfil = '',
+        linkedin = '',
+        lattes = '',
+    },
+    {
+        id = 4,
+        nome = '',
+        img_perfil = '',
+        linkedin = '',
+        lattes = '',
+    },
+    {
+        id = 5,
+        nome = '',
+        img_perfil = '',
+        linkedin = '',
+        lattes = '',
+    },
+    {
+        id = 6,
+        nome = '',
+        img_perfil = '',
+        linkedin = '',
+        lattes = '',
+    },
+    {
+        id = 7,
+        nome = '',
+        img_perfil = '',
+        linkedin = '',
+        lattes = '',
+    },
+    {
+        id = 6,
+        nome = '',
+        img_perfil = '',
+        linkedin = '',
+        lattes = '',
+    },
+]
+// definindo as variaveis para estudantes de ciências ambietais
+var bach_ambientais = [
+    {
+        id = 1,
+        nome = '',
+        img_perfil = '',
+        linkedin = '',
+        lattes = '',
+    },
+    {
+        id = 2,
+        nome = '',
+        img_perfil = '',
+        linkedin = '',
+        lattes = '',
+    },
+    {
+        id = 3,
+        nome = '',
+        img_perfil = '',
+        linkedin = '',
+        lattes = '',
+    },
+    {
+        id = 4,
+        nome = '',
+        img_perfil = '',
+        linkedin = '',
+        lattes = '',
+    },
+    {
+        id = 5,
+        nome = '',
+        img_perfil = '',
+        linkedin = '',
+        lattes = '',
+    },
+    {
+        id = 6,
+        nome = '',
+        img_perfil = '',
+        linkedin = '',
+        lattes = '',
+    },
+    {
+        id = 7,
+        nome = '',
+        img_perfil = '',
+        linkedin = '',
+        lattes = '',
+    },
+    {
+        id = 8,
+        nome = '',
+        img_perfil = '',
+        linkedin = '',
+        lattes = '',
+    },
+]
+// criando as variaveis de seleção - coordenadores
+var fotoCor = document.getElementById('foto-perfil-coordenadora');
+var nomeCor = document.getElementById('foto-perfil-coordenadora');
+var linkCor = document.getElementById('linkedin-coordenadora');
+var lattCor = document.getElementById('lattes-coordenadora');
+// criando as variaveis de seleção - pesquisadores internos
+var fotoPesqInt = document.getElementById('foto-perfil-pesq-int');
+var nomePesqInt = document.getElementById('nome-pesq-int');
+var linkPesqInt = document.getElementById('linkedin-pesq-int');
+var lattPesqInt = document.getElementById('lattes-pesq-int');
+// criando as variaveis de seleção - pesquisadores externos
+var fotoPesqExt = document.getElementById('foto-perfil-pesq-ext');
+var nomePesqExt = document.getElementById('nome-pesq-ext');
+var linkPesqExt = document.getElementById('linkedin-pesq-ext');
+var lattPesqExt = document.getElementById('lattes-pesq-ext');
+// criando as variaveis de seleção - ciências econômicas
+var fotoBachEco = document.getElementById('foto-perfil-bach-econ');
+var nomeBachEco = document.getElementById('nome-bach-econ');
+var linkBachEco = document.getElementById('linkedin-bach-econ');
+var lattBachEco = document.getElementById('lattes-bach-econ');
+// criando as variaveis de seleção - ciências ambientais
+var fotoBachAmb = document.getElementById('foto-perfil-bach-ambet');
+var nomeBachAmb = document.getElementById('nome-bach-amb');
+var linkBachAmb = document.getElementById('linkedin-bach-amb');
+var lattBachAmb = document.getElementById('lattes-bach-amb');
 
-    }
-}
+
