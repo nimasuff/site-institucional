26ba083dde0b2bdd0413a2292ea91ad67b026316
jgabrielgm <jgabrielgm1@gmail.com>
2021-04-21 21:18:27 -0300

    Mudanças sugeridas durante a reunião de 21/04/21

--- membros.js
+++ membros.js
@@ -1,5 +1,12 @@
 function esquerdaPerfil(){
-    var pesquisadores_uff = ['Alessandro Velário','Emmanoel de Oliveira','Manoel Tabet Soriano','Roldan Petros Muradian Sarache','Teresa Bezerra Meira', 'Carolina Grottera']
+    var membro_AV = {
+        nome_completo = 'Alessandro Valério',
+        foto_perfil = '',
+        linkedin = '',
+        lattes = 'http://lattes.cnpq.br/8542656790521330'
+    }
+    
+    var nome_pesq_int = ['Alessandro Velário','Emmanoel de Oliveira','Manoel Tabet Soriano','Roldan Petros Muradian Sarache','Teresa Bezerra Meira', 'Carolina Grottera']
     var i;
     for (i = 0; ){
 
